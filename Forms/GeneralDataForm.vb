ad60c8f62f7d5bf5e5e4831b88fc31490255c34d
Darian Gomez <gomezdarian1989@gmail.com>
2017-11-14 12:18:57 -0500

    deleting Blast

--- Forms/GeneralDataForm.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class GeneralDataForm
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Me.Visible = False
-    End Sub
-End Class
